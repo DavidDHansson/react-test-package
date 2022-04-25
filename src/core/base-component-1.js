f14b36b62e6ea2a55457cd322d3c3588a9fe05bf
David Hansson <hansson.d.david@gmail.com>
2022-04-25 19:53:14 +0200

    Version 1.0.5

--- src/core/base-component-1.js
+++ src/core/base-component-1.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function BaseComponent1() {
+export function BaseComponent1() {
     return (
-        <div style={{backgroundColor: "#ee22ff", padding: "10px", border: "2px solid black"}}>base-component-1</div>
+        <div style={{backgroundColor: "#00FF00", padding: "10px", border: "2px solid black"}}>base-component-1</div>
     )
 }
